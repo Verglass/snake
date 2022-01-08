26a1a7ad885f891ad625db0e7ffc40dd7805f435
Verglass <dimonidias@gmail.com>
2022-01-09 00:04:24 +0100

    functional at last

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,74 +1,96 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-function Board() {
-  const SIZE = 10
-  const STARTX = 5
-  const STARTY = 5
-  const FRAMERATE = 1
-  
-  const RIGHT = 0
-  const DOWN = 1
-  const LEFT = 2
-  const UP = 3
+const SIZE = 50
 
+function Snake(x, y) {
+  const positionX = x
+  const positionY = y
 
-  const [board, setBoard] = useState([])
-  const [snake, setSnake] = useState([])
-  const [direction, setDirection] = useState(RIGHT)
-
-  useEffect(() => {
-    init()
-    setInterval(() => {
-      move()
-    }, 1000 / FRAMERATE) 
-  }, [])
-
-  function init() {
-    setBoard(Array.from(Array(SIZE).fill().map((_, rowIndex) => {
-      return Array.from(Array(SIZE).fill().map((_, cellIndex) => {
-        return [cellIndex, rowIndex, 'cell']
-      }))
-    })))
-    setSnake(Array.from(Array(3).fill().map((_, index) => {
-      return [STARTX - index, STARTY, RIGHT]
-    })))
+  const style = {
+    left: positionX * SIZE,
+    top: positionY * SIZE,
   }
 
-  function checkState() {
-    board.forEach((row) => {
-      row.forEach((cell) => {
-        cell[2] = 'cell'
-        snake.forEach((part) => {
-          if (cell[0] === part[0] && cell[1] === part[1]) {
-            cell[2] = 'cell snake'
-          }
-        })
-      })
-    })
+  return (
+    <div className='snake' style={style}>
+    </div>
+  )
+}
+
+function Board() {
+  const [direction, setDirection] = useState('right')
+  const [snake, setSnake] = useState([
+    [1, 2],
+    [2, 2],
+    [3, 2]
+  ])
+
+  function handle(e) {
+    switch (e.keyCode) {
+      case 38:
+        setDirection('up')
+        break;
+      case 39:
+        setDirection('right')
+        break;
+      case 40:
+        setDirection('down')
+        break;
+      case 37:
+        setDirection('left')
+        break;
+    }
   }
 
   function move() {
+    const head = snake[snake.length - 1]
+    let newHead = [head[0], head[1]]
+
+    switch (direction) {
+      case 'up':
+        newHead[1] -= 1
+        break;
+      case 'right':
+        newHead[0] += 1
+        break;
+      case 'down':
+        newHead[1] += 1
+        break;
+      case 'left':
+        newHead[0] -= 1
+        break;
+    }
+
+    setSnake((snake) => {
+      let tmp = [...snake, newHead]
+      tmp.shift()
+      return tmp
+    })
   }
 
-  function DrawBoard() {
-    checkState()
-    return (
-      <div className='board'>
-        {board.map((row) => 
-          <div key={row}>
-            {row.map((cell) => 
-              <div className={cell[2]} key={cell}>
-                {cell[0]}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    document.addEventListener('keydown', handle)
+  }, [])
 
-  return <DrawBoard />
+  useEffect(() => {
+    const t = setInterval(() => {
+      move()
+    }, 1000)
+
+    return () => {
+      clearInterval(t)
+    }
+  }, [move])
+
+  return (
+    <div className='board'>
+      {snake.map((cell) => {
+        return Snake(cell[0], cell[1])
+      })}
+    </div>
+
+  )
 }
 
 function App() {
